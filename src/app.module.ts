5f6fb58e8a02b0c607a0294dc7f882c978e0f9bf
JiaPeng <597683167@qq.com>
2025-08-24 20:46:29 +0800

    feat(env): 添加环境变量配置并初始化项目结构
    - 移除 .env.development 文件
    - 更新 .gitignore 文件，统一环境变量配置
    - 删除 .prettierrc 文件
    - 更新 app.module.ts，添加 ConfigModule、AuthModule 和 PrismaModule
    - 更新 app.service.ts，注入 PrismaService
    - 更新 package.json，添加 @nestjs/config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { ConfigModule } from "@nestjs/config";
+import { AuthModule } from "./auth/auth.module";
+import { PrismaModule } from "./prisma/prisma.module";
+import process from "process";
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `.env.${process.env.ENV}`,
+    }),
+    PrismaModule,
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,10 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from "@nestjs/common";
+import { PrismaService } from "./prisma/prisma.service";
 
 @Injectable()
 export class AppService {
+  constructor(private prisma: PrismaService) {}
   getHello(): string {
-    return 'Hello World!';
+    return "Hello World!";
   }
 }
