fae3c729743af916771d2acd813e6740d4512f2b
JiaPeng <597683167@qq.com>
2025-08-25 17:47:23 +0800

    feat(middleware): 添加请求 ID 中间件
    - 在 src/main.ts 中引入并使用请求 ID 中间件
    - 重构 src/middleware/requestId.middleware.ts，简化为纯函数形式
    - 此中间件为每个请求生成唯一的请求 ID，并

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,13 @@ import { AppModule } from "./app.module";
 import process from "process";
 import { NestExpressApplication } from "@nestjs/platform-express";
 import { ValidationPipe } from "@nestjs/common";
+import { requestIdMiddleware } from "./middleware";
 
 (async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule, {
     logger: ["error", "warn"],
   });
+  app.use(requestIdMiddleware);
   app.setGlobalPrefix("api");
   app.useGlobalPipes(
     new ValidationPipe({
--- src/middleware/requestId.middleware.ts
+++ src/middleware/requestId.middleware.ts
@@ -1,14 +1,10 @@
-import { Injectable, NestMiddleware } from "@nestjs/common";
 import { Request, Response, NextFunction } from "express";
 import { v7 as uuidv7 } from "uuid";
 
-@Injectable()
-export class RequestIdMiddleware implements NestMiddleware {
-  use(req: Request, res: Response, next: NextFunction) {
-    const requestId = uuidv7();
+export function requestIdMiddleware(req: Request, res: Response, next: NextFunction) {
+  const requestId = uuidv7();
 
-    res.setHeader("X-Request-Id", requestId);
+  res.setHeader("X-Request-Id", requestId);
 
-    next();
-  }
+  next();
 }
