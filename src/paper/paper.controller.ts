7a5eff13050b466dabe5e382a7ee3dd42575dcae
JiaPeng <597683167@qq.com>
2025-09-13 09:28:41 +0800

    feat(paper): 添加试卷查询功能
    - 新增 GetPaperDto 类用于试卷查询参数验证
    - 在 PaperController 中添加 getPaper 方法处理 中实现 getPaper 方法，用于获取指定级别的试卷
    - 优化了纸张查询逻辑，增加了对查询参数的验证和错误处理

--- src/paper/paper.controller.ts
+++ src/paper/paper.controller.ts
@@ -1,11 +1,13 @@
 import { Controller, Get, Param, Post, Query, Res } from "@nestjs/common";
 import { PaperService } from "./paper.service";
 import { GetPaperDto, GetPartsDto, GetSectionDto } from "./paper.dto";
+import { Public, ResponseMessage } from "../decorator";
 
 @Controller("paper")
 export class PaperController {
   constructor(private paperService: PaperService) {}
 
+  @Public()
   @Get("list")
   async getList() {
     return this.paperService.getList();
